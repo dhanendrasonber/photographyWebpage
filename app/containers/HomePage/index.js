bb55882311eb4468f51f7366bdb75724d670a543
Vaughan Stedman <vaughan.stedman@gmail.com>
2018-02-07 09:02:52 +1100

    Inclusion of Redux Saga

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,7 +11,7 @@
 
 import React from 'react';
 import PhotoCard from 'components/PhotoCard';
-import HeaderBar from 'components/HeaderBar';
+// import HeaderBar from 'components/HeaderBar';
 import { Grid, Row } from 'react-bootstrap/lib';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
@@ -30,7 +30,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
             <Row>
               <PhotoCard key="a" source="http://vaughanstedman.me/a2a7e31342fa30e92761e3996b0403d8.jpg" />
               <PhotoCard key="b" source="http://vaughanstedman.me/b3ae6362db9706d6eb1de8c202f1956f.jpg" />
-              <PhotoCard key="c" source="http://vaughanstedman.me/a2a7e31342fa30e92761e3996b0403d8.jpg" />
+              <PhotoCard key="c" source="https://drive.google.com/uc?export=view&id=1EYOolrJoOvkZHV9bL1jkaGl9-57H6NFT" />
               <PhotoCard key="d" source="http://vaughanstedman.me/b3ae6362db9706d6eb1de8c202f1956f.jpg" />
             </Row>
           </Grid>
---
+++ app/containers/HomePage/saga.js
@@ -0,0 +1,2 @@
+import { takeLatest } from 'redux-saga';
+import { take, call, put, cancel, select } from 'redux-saga/effects';
