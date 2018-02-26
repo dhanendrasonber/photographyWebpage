865e5b53f4ece443bb3eb39610d58bdf59bd9eb0
vpste1 <vaughan.stedman@gmail.com>
2018-02-26 20:22:04 +1100

    packages update

--- app/components/PhotoCard/tests/index.test.js
+++ app/components/PhotoCard/tests/index.test.js
@@ -1,10 +1,24 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import PhotoCard from '../index';
+import PhotoCard from '../index';
 
 describe('<PhotoCard />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('Renders a <div>', () => {
+    const renderedComponent = shallow(
+      <PhotoCard />
+    );
+    expect(
+      renderedComponent.find('div').node
+    ).toBeDefined();
+  });
+  it('Renders an <Image>', () => {
+    const renderedComponent = shallow(
+      <PhotoCard />
+    );
+    console.log(renderedComponent);
+    expect(
+      renderedComponent.find('img').node
+    ).toBeDefined();
   });
 });
